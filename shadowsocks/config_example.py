1053b642e23eb6588c1c6df4c974e8dc504a0fed
dylanchu <chdy.uuid@gmail.com>
2019-06-12 07:51:35 +0800

    initial commit
        based on fsgmhoward's commit 293b2ae

---
+++ shadowsocks/config_example.py
@@ -0,0 +1,98 @@
+# !!! Please rename config_example.py as config.py BEFORE editing it !!!
+
+import logging
+# !!! Only edit this line when you update your configuration file !!!
+# After you update, the value of CONFIG_VERSION in config.py and
+# config_example.py should be the same in order to start the server
+CONFIG_VERSION = 1
+
+
+# Manyuser Interface Settings
+# ---------------------------
+# If API is enabled, database will no longer be used
+# The known web panel that is compatible with the API is SS-Panel V3
+# Be careful and check whether your web panel supports this API BEFORE you enable this feature
+API_ENABLED = False
+# Time interval between 2 pulls from the database or API
+CHECKTIME = 30
+# Time interval between 2 pushes to the database or API
+SYNCTIME = 300
+# Timeout for MySQL connection or web socket (if using API)
+TIMEOUT = 30
+
+# MySQL Database Config (NO NEED to edit if you set API_ENABLED 'True' above)
+MYSQL_HOST = 'db.example.net'
+MYSQL_PORT = 3306
+MYSQL_USER = 'root'
+MYSQL_PASS = 'root'
+MYSQL_DB = 'shadowsocks'
+MYSQL_USER_TABLE = 'user'
+
+# Shadowsocks MultiUser API Settings
+API_URL = 'http://domain/mu'
+# API Key (you can find this in the .env file if you are using SS-Panel V3)
+API_PASS = 'mupass'
+API_NODE_ID = '1'
+
+
+# Manager Settings
+# ----------------
+# USUALLY you can just keep this section unchanged
+# if you want manage in other server you should set this value to global ip
+MANAGER_BIND_IP = '127.0.0.1'
+# make sure this port is idle
+MANAGER_PORT = 65000
+
+
+# Server Settings
+# ---------------
+# Address binding settings
+# if you want to bind ipv4 and ipv6 please use '::'
+# if you want to bind only all of ipv4 please use '0.0.0.0'
+# if you want to bind a specific IP you may use something like '4.4.4.4'
+SS_BIND_IP = '::'
+# This default method will be replaced by database/api query result if applicable when SS_CUSTOM_METHOD is enabled
+SS_METHOD = 'chacha20-ietf-poly1305'
+SS_CUSTOM_METHOD = True
+# Enforce the use of AEAD ciphers
+# When enabled, all requests of creating server with a non-AEAD cipher will be omitted
+# For more information, please refer to http://www.shadowsocks.org/en/spec/AEAD-Ciphers.html
+SS_ENFORCE_AEAD = False
+# Skip listening these ports
+SS_SKIP_PORTS = [80]
+# TCP Fastopen (Some OS may not support this, Eg.: Windows)
+SS_FASTOPEN = False
+# Shadowsocks socket timeout
+# It should not be too small as some protocol has keep-alive packet of long time, Eg.: BT
+SS_TIMEOUT = 310
+
+
+# Firewall Settings
+# -----------------
+# These settings are to prevent user from abusing your service
+SS_FIREWALL_ENABLED = False
+# Mode = whitelist or blacklist
+SS_FIREWALL_MODE = 'blacklist'
+# Member ports should be INTEGERS
+# Only Ban these target ports (for blacklist mode)
+SS_BAN_PORTS = [22, 23, 25]
+# Only Allow these target ports (for whitelist mode)
+SS_ALLOW_PORTS = [53, 80, 443, 8080, 8081]
+# Trusted users (all target ports will be not be blocked for these users)
+SS_FIREWALL_TRUSTED = [443]
+# Banned Target IP List
+SS_FORBIDDEN_IP = []
+
+
+# Debugging and Logging Settings
+# --------------------------
+# If SS_VERBOSE is true, traceback will be printed to STDIO when an exception is thrown
+SS_VERBOSE = False
+LOG_ENABLE = True
+# Available Log Level: logging.NOTSET|DEBUG|INFO|WARNING|ERROR|CRITICAL
+LOG_LEVEL = logging.INFO
+LOG_FILE = 'shadowsocks.log'
+# The following format is the one suggested for debugging
+# LOG_FORMAT = '%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s'
+LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
+LOG_DATE_FORMAT = '%b %d %H:%M:%S'
---
+++ shadowsocks/servers.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 mengskysama
+# Copyright 2016 Howard Liu
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import sys
+import os
+import logging
+import time
+# Check whether the config is correctly renamed
+try:
+    import config
+except ImportError:
+    print('[ERROR] Please rename `config_example.py` to `config.py` first!')
+    sys.exit('config not found')
+# For those system do not have thread (or _thread in Python 3)
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+
+logger = logging.getLogger()
+logger.setLevel(config.LOG_LEVEL)
+consoleHandler = logging.StreamHandler(stream=sys.stdout)
+consoleHandler.setFormatter(logging.Formatter(config.LOG_FORMAT, datefmt=config.LOG_DATE_FORMAT))
+consoleHandler.setLevel(config.LOG_LEVEL)
+
+from sys import platform
+if platform == 'linux' or platform == 'linux2':
+    with open('/proc/1/cgroup', 'rt') as ifh:
+        if 'docker' in ifh.read():
+            print('[INFO] Running inside a docker.')
+            print('[INFO] Log file config will be ignored & log will not be printed to stdout.')
+            config.LOG_FILE = 'shadowsocks.log'
+        else:
+            logger.addHandler(consoleHandler)
+
+if config.LOG_ENABLE:
+    # If enabled logging to file, add a fileHandler as well
+    if sys.version_info >= (2, 6) and platform != 'win32':
+        # If python version is >= 2.6 and it is not running on Windows, use WatchedFileHandler
+        import logging.handlers
+        fileHandler = logging.handlers.WatchedFileHandler(config.LOG_FILE)
+    else:
+        fileHandler = logging.FileHandler(config.LOG_FILE)
+    fileHandler.setFormatter(logging.Formatter(config.LOG_FORMAT, datefmt=config.LOG_DATE_FORMAT))
+    fileHandler.setLevel(config.LOG_LEVEL)
+    logger.addHandler(fileHandler)
+
+# Check whether the versions of config files match
+try:
+    import config_example
+    if not hasattr(config, 'CONFIG_VERSION') or config.CONFIG_VERSION != config_example.CONFIG_VERSION:
+        logging.error('Your config file is outdated. Please update `config.py` according to `config_example.py`.')
+        sys.exit('config out-dated')
+except ImportError:
+    logging.error('DO NOT delete the example configuration! Please re-upload it or use `git reset` to recover the file!')
+    sys.exit('example config file missing')
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
+import manager
+import traceback
+from dbtransfer import DbTransfer
+
+if os.path.isdir('../.git') and not os.path.exists('../.nogit'):
+    import subprocess
+    if "check_output" not in dir(subprocess):
+        # Compatible with Python < 2.7
+        VERSION = subprocess.Popen(["git", "describe", "--tags", "--always"], stdout=subprocess.PIPE).communicate()[0]
+    else:
+        VERSION = subprocess.check_output(["git", "describe", "--tags", "--always"])
+    # Remove EOL characters in git's output
+    VERSION = VERSION.rstrip()
+else:
+    VERSION = '3.4.0-dev'
+
+
+def subprocess_callback(stack, exception):
+    logging.info('Exception thrown in %s: %s' % (stack, exception))
+    if config.SS_VERBOSE:
+        traceback.print_exc()
+
+
+def main():
+    if config.SS_FIREWALL_ENABLED:
+        if config.SS_FIREWALL_MODE == 'blacklist':
+            firewall_ports = config.SS_BAN_PORTS
+        else:
+            firewall_ports = config.SS_ALLOW_PORTS
+    else:
+        firewall_ports = None
+
+    config_passed = {
+        'server': config.SS_BIND_IP,
+        'local_port': 1081,
+        'port_password': {},
+        'method': config.SS_METHOD,
+        'manager_address': '%s:%s' % (config.MANAGER_BIND_IP, config.MANAGER_PORT),
+        'timeout': config.SS_TIMEOUT,
+        'fast_open': config.SS_FASTOPEN,
+        'verbose': config.SS_VERBOSE,
+        'forbidden_ip': config.SS_FORBIDDEN_IP,
+        'firewall_mode': config.SS_FIREWALL_MODE,
+        'firewall_trusted': config.SS_FIREWALL_TRUSTED,
+        'firewall_ports': firewall_ports,
+        'aead_enforcement': config.SS_ENFORCE_AEAD
+    }
+    logging.info('-----------------------------------------')
+    logging.info('Multi-User Shadowsocks Server Starting...')
+    logging.info('Current Server Version: %s' % VERSION)
+    if config.API_ENABLED:
+        logging.info('Now using MultiUser API as the user interface')
+    else:
+        logging.info('Now using MySQL Database as the user interface')
+    logging.info('Now starting manager thread...')
+    thread.start_new_thread(manager.run, (config_passed, subprocess_callback,))
+    time.sleep(5)
+    logging.info('Now starting user pulling thread...')
+    thread.start_new_thread(DbTransfer.thread_pull, ())
+    time.sleep(5)
+    logging.info('Now starting user pushing thread...')
+    thread.start_new_thread(DbTransfer.thread_push, ())
+    
+    while True:
+        time.sleep(100)
+
+
+if __name__ == '__main__':
+    main()
